92ae1fff80cfcbbd1c35d9fa9f21f679d3d26f7c
fares8lefi <fareslefi4@gmail.com>
2026-04-12 00:18:00 +0200

    chore: set up environment variables with .env

--- src/main/java/Utils/Database.java
+++ src/main/java/Utils/Database.java
@@ -8,9 +8,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Database {
     private static final Dotenv dotenv = Dotenv.load();
-    private   final String URl = dotenv.get("DB_URL");
-    private final String USERNAME = dotenv.get("DB_USERNAME");
-    private final String PASSWORD = dotenv.get("DB_PASSWORD");
+    private final String URL = dotenv.get("DB_URL");
+    private final String USERNAME = dotenv.get("DB_USER");
+    private final String PASSWORD = dotenv.get("DB_PASS");
     private Connection connection;
     private  static Database instance ;
 
@@ -27,9 +27,11 @@ public class Database {
 
     private Database() {
         try {
-            //
+            System.out.println("URL: " + URL);
+            System.out.println("USERNAME: " + USERNAME);
+            System.out.println("PASSWORD: " + PASSWORD);
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URl, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("--- Connexion réussie à la base de données ! ---");
         } catch (ClassNotFoundException e) {
             System.out.println("ERREUR : Driver MySQL non trouvé. Vérifiez que le JAR est dans le classpath.");
