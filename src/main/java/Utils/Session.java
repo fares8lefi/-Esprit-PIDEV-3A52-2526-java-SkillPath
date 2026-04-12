2921577b5dd271f6eb667cb92e6be6f47f08a2b1
fares8lefi <fareslefi4@gmail.com>
2026-04-12 02:02:43 +0200

    feat: add session class

---
+++ src/main/java/Utils/Session.java
@@ -0,0 +1,23 @@
+package Utils;
+
+import Models.User;
+
+public class Session {
+    private static User currentUser;
+
+    public static void login(User user) {
+        currentUser = user;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+}
